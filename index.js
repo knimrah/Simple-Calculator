11e664d3eb3ca4b4a1ce744bff7455a577680973
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-03-28 17:33:17 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{ message: 'Enter first number', type: 'number', name: 'firstnumber' },
+    { message: 'Enter second number', type: 'number', name: 'Secondnumber' },
+    { message: 'select one of the operators to perform action', type: 'list', name: 'operator',
+        choices: ['Addition', 'Substraction', 'Multiplication', 'Division'] }
+]);
+if (answer.operator === 'Addition') {
+    console.log(answer.firstnumber + answer.Secondnumber);
+}
+else if (answer.operator === 'Substraction') {
+    console.log(answer.firstnumber - answer.Secondnumber);
+}
+else if (answer.operator === 'Multiplication') {
+    console.log(answer.firstnumber * answer.Secondnumber);
+}
+else if (answer.operator === 'Division') {
+    console.log(answer.firstnumber / answer.Secondnumber);
+}
+else {
+    console.log('Please select valid operator.');
+}
